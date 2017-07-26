79d968130837e140f43568a28d2cc8f399847fbf
Charles Renk <renkx008@umn.edu>
2017-07-26 10:13:13 -0500

    Objects by reference and by value

--- script.js
+++ script.js
@@ -2,18 +2,31 @@ console.log("script.js has been sourced in")
 
 // [name,favoriteAnimal]
 var people = [
-    ["Canin",'Uniflitten'],
-    ["Charly",'Angler fish'],
-    ["Bobby",'Liger'],
-    ["Luke",'Killer Whale'],
-];
+    {
+        name: "Canin",
+        favoriteAnimal: 'Uniflitten',
+    },
+    {
+        name:"Charly",
+        favoriteAnimal: 'Angler fish',
+    },
+    {
+        name: "Bobby",
+        favoriteAnimal: 'Liger',
+    },
+    {
+        name: "Luke",
+        favoriteAnimal: 'Killer Whale'
+    },  
+]
+
 
 console.log(people)
 
 for (var i = 0; i < people.length; i++){
     var childPersonArray = people[i]
-    console.log(childPersonArray[0]);
+    console.log(childPersonArray[0] + "'s favorite animal is the " + childPersonArray[1]);
 
-}
+};
 //log {person}'s favroite animal is the [favoriteAnimal]
 //log for Canin: Canin's favorite animal is the Uniflitten
